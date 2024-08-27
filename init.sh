39bcc7c6512798a342779c00e4e0c28d9791487b
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-27 22:58:03 +0100

    ar(init) icecast

--- init.sh
+++ init.sh
@@ -87,8 +87,8 @@ firewall_dir="/etc/fail2ban"
 if [ -d "$firewall_dir" ]; then
 	echo "dp::euterpe::firewall::(busy)::copying configs to fail2ban."
 	cd firewall
-	cp _jail.local "$firewall_dir/jail.d/dp-patience.local"
-	cp _*.conf "$firewall_dir/filter.d"
+	cp _dp-patience.local "$firewall_dir/jail.d/dp-patience.local"
+	cp _dp-patience.conf "$firewall_dir/filter.d/dp-patience.conf"
 else
 	echo "dp::euterpe::firewall::(error)::fail2ban seems to still be missing ::mystery-tune-plays::."
 	exit 1;
